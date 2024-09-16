a94a38e13c47188f774e84ecacf1dc7dc8d089ed
Kathleen Jogno <jognokath@gmail.com>
2024-09-16 16:48:39 +0800

    fix issue Content-Security-Policy

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,20 @@ const nextConfig = {
   basePath: "/portfolio",
   output: "export",
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // Matching all routes
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; img-src 'self' https://redkath.github.io; script-src 'self'; style-src 'self';", // Adjust as needed
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
