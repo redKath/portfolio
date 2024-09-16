50666b60fe1b1c3a19e4078ba9a6e925f1ecf58a
Kathleen Jogno <jognokath@gmail.com>
2024-09-16 16:52:29 +0800

    fix issue

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,15 @@ const nextConfig = {
   basePath: "/portfolio",
   output: "export",
   reactStrictMode: true,
-  async headers() {
-    return [
+  images: {
+    remotePatterns: [
       {
-        // Matching all routes
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; img-src 'self' https://redkath.github.io; script-src 'self'; style-src 'self';", // Adjust as needed
-          },
-        ],
+        protocol: 'https',
+        hostname: 'redkath.github.io',
+        port: '',
+        pathname: '/porfolio/**',
       },
-    ];
+    ],
   },
 };
 
